bc3e521a27de96c4e81606ff169cb287ffb042d8
unknown <johnattamedeiros@gmail.com>
2024-08-09 14:18:36 -0300

    Adding icos and top baiter and stats

--- src/api/playerService.js
+++ src/api/playerService.js
@@ -1,9 +1,30 @@
-const API_URL = 'http://localhost:3000/players'; // Substitua pelo URL real da API
+const API_URL = 'http://mixdurasso.vps-kinghost.net:3000/players';
 
 export const fetchPlayers = async () => {
   const response = await fetch(API_URL);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  return response.json();
+  let players = await response.json();
+
+
+
+  const responseBaiter = await fetch("http://mixdurasso.vps-kinghost.net:3000/players/stat?sort=how_baiter&order=asc");
+  if (!responseBaiter.ok) {
+    throw new Error('Network response was not ok');
+  }
+  let topBaiters = await responseBaiter.json();
+
+  for (let i = 0; i < players.length; i++) {
+    if(players[i]._id === topBaiters[0]._id){
+      players[i].topBaiter = 1;
+    }
+    if(players[i]._id === topBaiters[1]._id){
+      players[i].topBaiter = 2;
+    }
+    if(players[i]._id === topBaiters[2]._id){
+      players[i].topBaiter = 3;
+    }
+  }
+  return players;
 };
--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import './../styles/gc.css'
 
 const PlayerCard = ({ player }) => {
   return (
     <div className="player-card">
       <img src={`https://static.gamersclub.com.br/players/avatar/${player.id_gc}/${player.id_gc}_full.jpg`} alt={`${player.nick}'s photo`} className="player-photo" />
 
-      
+
       <div className="player-info">
         <h3 className="player-name">{player.nick}</h3>
-        <p className="player-stats">Kills: {player.Matou}</p>
-        <p className="player-stats">Deaths: {player.Morreu}</p>
-        <p className="player-stats">Score: {player.KDR}</p>
+        <h4> 
+
+        
+          <span data-v-551cb209="" class={`gcf-new-badge-level gcf-new-badge-level-${player.level}`}>
+            <span data-v-551cb209="" class="gcf-badge-level-value">{player.level}
+           </span></span>
+           
+        </h4>
+
+        <div className="player-stats"> <img src="/kill.png" class="stats-ico"/> {player.Matou}   <img src="/death.png"class="stats-ico"/> {player.Morreu} <img src="/kdr.png"class="stats-ico"/>{player.KDR}</div>
+        
+        {player.topBaiter && <div className="player-stats-t"> <img src="/bait.png" class="stats-ico white" /> TOP {player.topBaiter}</div>}
+        
       </div>
     </div>
   );
